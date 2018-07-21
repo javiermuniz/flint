15bdc5348460d1b877fccf23771c3b9d22b6dcf8
Javier Muniz <javier.muniz@zetaminusone.com>
2018-07-21 13:55:28 -0600

    work on pricing and home page

--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -4,11 +4,15 @@ import { CommonModule } from '@angular/common';
 import { LandingRoutingModule } from './landing-routing.module';
 import { HomeComponent } from './home/home.component';
 import { PricingComponent } from './pricing/pricing.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
-    LandingRoutingModule
+    LandingRoutingModule,
+    FlexLayoutModule,
+    MatCardModule
   ],
   declarations: [HomeComponent, PricingComponent]
 })
